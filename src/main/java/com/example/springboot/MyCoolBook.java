56cbca965c0c2e0901e3feed2f7dad59c2fa9320
Marco Hassan <marco.hassan@zurich.com>
2022-02-08 13:52:31 +0100

    my first commit. here is a very minimal spring app. tested swagger integration. very nice can well document your APIs in such a way.

---
+++ src/main/java/com/example/springboot/MyCoolBook.java
@@ -0,0 +1,24 @@
+package com.example.springboot;
+
+public class MyCoolBook {
+	private String Cover;
+	private String Rocket;
+	
+	public String getCover() {		
+		return this.Cover;
+	}
+	
+	public String getRocket() {		
+		return this.Rocket;
+	}
+	
+	public void setCover(String MyName) {		
+		this.Cover = MyName;
+	}
+	
+	public void setRocket(String MyName) {		
+		this.Rocket = MyName;
+	}
+	
+
+}
