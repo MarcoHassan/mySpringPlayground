b8f97ea21db0e3c9c8f426e1606773e3db3814f7
Marco Hassan <marco.hassan@zurich.com>
2022-03-31 09:47:27 +0200

    making order in the project. starting to split it in logical blocks. reformulating build.gradle

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -3,8 +3,10 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication @ComponentScan(basePackages = { "com.example.swagger","com.example.blob"} )
+
 public class Application {
 
     public static void main(String[] args) {
