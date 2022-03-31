9946c3acf9111afd3e7a5487d922580d45549e23
Marco Hassan <marco.hassan@zurich.com>
2022-03-31 09:38:36 +0200

    making order in the project. starting to split it in logical blocks. reformulating build.gradle

---
+++ src/main/java/com/example/reactor/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package holds some tests for working in a full reactive way. 
+ * <p>
+ * It explores a bit the spring reactor project
+ */
+
+package com.example.reactor;
---
+++ src/main/java/com/example/springboot/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package holds the entry point for the springboot application. 
+ * <p>
+ * Keep it split in order to keep order across the broader application. 
+ */
+
+package com.example.springboot;
