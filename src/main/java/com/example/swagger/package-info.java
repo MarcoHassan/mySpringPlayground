98bc2413b522e5d971cbc8b33a7fd877c8815651
Marco Hassan <marco.hassan@zurich.com>
2022-04-13 12:15:27 +0200

    added reference for swaggerui usage

--- src/main/java/com/example/swagger/package-info.java
+++ src/main/java/com/example/swagger/package-info.java
@@ -1,7 +1,9 @@
 /**
  * Here you test 101 features in order to create cool swagger documented endpoints. 
  * <p>
- * The focus is on the rendering of the endpoints on Swagger in order to keep the interfaces maintainable. 
+ * The focus is on the rendering of the endpoints on Swagger in order to keep the interfaces maintainable.
+ * <p>
+ * Check as well this project {@link https://github.com/lankydan/spring-rest-api-with-swagger/tree/main/src/main/java/dev/lankydan/people} 
  */
 
 package com.example.swagger;
