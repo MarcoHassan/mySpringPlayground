4766f813cbf6cc1736a033bdac6ac6cb7424609e
Marco Hassan <marco.hassan@zurich.com>
2022-04-19 11:23:36 +0200

    proper bean configuration. can keep it in your app module. just have to add componentscan

--- src/main/java/com/example/springboot/BeanConfiguration.java
+++ src/main/java/com/example/springboot/BeanConfiguration.java
@@ -1,10 +1,14 @@
-package com.example.controller;
+package com.example.springboot;
 
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
+@SpringBootConfiguration
+@ComponentScan // Have to explicitly tell Spring to scan this configuration such that you will find it as a Bean
+               // See more here: https://www.baeldung.com/spring-component-scanning
 public class BeanConfiguration {
 	
     @Bean
